68f8f8ce21dba1c691818f52b492822b1d86e320
Xyarok <126290197+Xyarok@users.noreply.github.com>
2024-06-30 14:14:18 -0400

    Add files via upload

---
+++ one-step_install.sh
@@ -0,0 +1,20 @@
+# Update to latest system
+sudo dnf upgrade
+
+# Install core dnf packages
+sudo dnf install kitty
+sudo dnf install waybar
+sudo dnf install wofi
+sudo dnf install zoxide
+sudo dnf install zsh
+
+# Install non core dnf packages
+sudo dnf install gimp
+
+# Upgrade flatpak
+sudo flatpak upgrade
+
+# Install flatpak packages
+sudo flatpak install floorp
+sudo flatpak install onlyoffice
+sudo flatpak install steam
