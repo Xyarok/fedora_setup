0ec821a51462dd22608bacb4719c8a9120e0fb86
Xyarok <126290197+Xyarok@users.noreply.github.com>
2024-07-03 21:11:17 -0400

    Update one-step_install.sh

--- one-step_install.sh
+++ one-step_install.sh
@@ -13,5 +13,9 @@ sudo flatpak upgrade
 # Install flatpak packages
 sudo flatpak install floorp onlyoffice steam
 
+if "sudo flatpak install floorp" == true {} (
+  sudo dnf remove firefox
+)
+
 # Install Oh My Zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
