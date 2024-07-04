2ea713ab27bdf22173f4c1c19c663d8a20719941
Xyarok <126290197+Xyarok@users.noreply.github.com>
2024-07-04 09:09:30 -0400

    Add files via upload

--- one-step_install.sh
+++ one-step_install.sh
@@ -1,21 +1,47 @@
-# Update to latest system
-sudo dnf upgrade
+#!/bin/bash
 
-# Install core dnf packages
-sudo dnf install kitty torbrowser-launcher waybar wofi zoxide zsh
+DNF_PACKAGE_INSTALL_LIST=(
+	kitty
+	torbrowser-launcher
+	vim
+	waybar
+	wofi
+	zoxide
+	zsh
+)
 
-# Install non core dnf packages
-sudo dnf install gimp
+FLATPAK_PACKAGE_INSTALL_LIST=(
+	floorp
+	onlyoffice
+	steam
+)
 
-# Upgrade flatpak
-sudo flatpak upgrade
+# Update Fedora
+sudo dnf upgrade -y
+
+# Update flatpak
+sudo flatpak update
+
+# Install dnf package
+for dnf_package_name in ${DNF_PACKAGE_INSTALL_LIST[@]}; do
+	if ! sudo dnf list --installed | grep -q "^\<$dnf_package_name\>"; then
+		echo "installed $dnf_package_name..."
+		sleep .5
+		sudo dnf install "$dnf_package_name" -yq
+		echo "$dnf_package_name installed"
+	else
+		echo "$dnf_package_name already installed"
+	fi
+done
 
 # Install flatpak packages
-sudo flatpak install floorp onlyoffice steam
-
-if "sudo flatpak install floorp" == true {} (
-  sudo dnf remove firefox
-)
+for flatpak_package_name in ${FLATPAK_PACKAGE_INSTALL_LIST[@]}; do
+	if ! flatpak list | grep -q $flatpak_package_name; then
+		flatpak install "$flatpak_package_name" -yq
+	else
+		echo "$flatpak_package_name already installed"
+	fi
+done
 
 # Install Oh My Zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
