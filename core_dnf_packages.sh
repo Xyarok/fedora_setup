6f03461fe162e51a4a8d0720e68f2778722942d7
Xyarok <126290197+Xyarok@users.noreply.github.com>
2024-07-14 12:28:37 -0400

    Add files via upload

---
+++ core_dnf_packages.sh
@@ -0,0 +1,19 @@
+DNF_CORE_PACKAGE_INSTALL_LIST=(
+        kitty
+        vim
+        waybar
+        wofi
+        zoxide
+        zsh
+)
+
+for dnf_core_package_name in ${DNF_CORE_PACKAGE_INSTALL_LIST[@]}; do
+        if ! sudo dnf list --installed | grep -q "^\<$dnf_core_package_name\>"; then
+                echo "installed $dnf_core_package_name..."
+                sleep .5
+                sudo dnf install "$dnf_core_package_name" -yq
+                echo "$dnf_core_package_name installed"
+        else
+                echo "$dnf_core_package_name already installed"
+        fi
+done
