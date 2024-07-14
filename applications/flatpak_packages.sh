5f4e9362fa899eeffe352066d439b411581c5db8
Xyarok <126290197+Xyarok@users.noreply.github.com>
2024-07-14 12:29:12 -0400

    Add files via upload

---
+++ applications/flatpak_packages.sh
@@ -0,0 +1,19 @@
+# flatpak packages
+FLATPAK_PACKAGE_INSTALL_LIST=(
+        floorp
+        obsidian
+        onlyoffice
+        protonvpn
+        steam
+        vscodium
+)
+
+# Install flatpak packages
+for flatpak_package_name in ${FLATPAK_PACKAGE_INSTALL_LIST[@]}; do
+        if ! flatpak list | grep -q $flatpak_package_name; then
+                flatpak install "$flatpak_package_name"
+        else
+                echo "$flatpak_package_name already installed"
+        fi
+done
+       
---
+++ applications/user_dnf_packages.sh
@@ -0,0 +1,23 @@
+DNF_USER_PACKAGE_INSTALL_LIST=(
+        gimp
+        obs-studio
+        torbrowser-launcher
+)
+
+for dnf_user_package_name in ${DNF_USER_PACKAGE_INSTALL_LIST[@]}; do
+        if ! sudo dnf list --installed | grep -q "^\<$dnf_user_package_name\>"; then
+                echo "installed $dnf_user_package_name..."
+                sleep .5
+                sudo dnf install "$dnf_user_package_name" -yq
+                echo "$dnf_user_package_name installed"
+                # Install oh my zsh
+                if ! sudo dnf list --installed | grep -q ${DNF_CORE_PACKAGE_INSTALL_LIST[@zsh]}; then
+                        sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+                        echo "oh my zsh successfully installed"
+                else
+                        echo "oh my zsh failed to install"
+                fi
+        else
+                echo "$dnf_user_package_name already installed"
+        fi
+done
