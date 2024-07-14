ae8f73f3aafb2d71d0335926882ba5555be10007
Xyarok <126290197+Xyarok@users.noreply.github.com>
2024-07-14 12:41:18 -0400

    Update flatpak_packages.sh

--- applications/flatpak_packages.sh
+++ applications/flatpak_packages.sh
@@ -8,6 +8,8 @@ FLATPAK_PACKAGE_INSTALL_LIST=(
         vscodium
 )
 
+sudo flatpak update
+
 # Install flatpak packages
 for flatpak_package_name in ${FLATPAK_PACKAGE_INSTALL_LIST[@]}; do
         if ! flatpak list | grep -q $flatpak_package_name; then
